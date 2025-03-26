4969fa3836a0095fd5ed2c33faab99cdbbb11821
pranav-cholleti <pranavcholleti@gmail.com>
2025-03-26 22:43:51 +0530

    modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,"/frontend/dist")));
-    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend','dist','index.html')));
+    app.use(express.static(path.join(__dirname,"/build")));
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'build','index.html')));
 }
 
 // Enable CORS (Configure this properly for production!)
